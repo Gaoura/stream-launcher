ffe19fd673e678b8f46b9eccabac175ecffc3cd3
Gaoura <gaoura@outlook.com>
2017-04-11 22:49:03 +0200

    Changing the way class methods are written

--- src/Launcher.rb
+++ src/Launcher.rb
@@ -7,7 +7,6 @@ class Launcher
 
    class << self
       private :new
-   end
 
 =begin REVIEW: Doesn't work, open give HTTPError 400 Bad Request
 
@@ -18,7 +17,7 @@ class Launcher
 # (CLI : "livestreamer --twitch-oauth-authenticate" ;
 # then config file : "twitch-oauth-token=xxxx or CLi : "livestreamer --twitch-oauth-token xxxx stream")
 # and just check the livestreamer output to know if stream is online or offline
-      def self.is_stream_online?(stream_url)
+      def is_stream_online?(stream_url)
          # TODO: Twitch functionality only, might add Dailymotion, Youtube Live
          name = get_streamer_name(stream_url)
          raise StreamLauncherException if name.nil?
@@ -29,45 +28,46 @@ class Launcher
       end
 =end
 
-   def self.stream_availables_qualities(stream_url, path_to_livestreamer = nil)
-      if path_to_livestreamer.nil?
-         cmd_output = `livestreamer #{stream_url}`
-      else
-         cmd_output = `#{path_to_livestreamer} #{stream_url}`
+      def stream_availables_qualities(stream_url, path_to_livestreamer = nil)
+         if path_to_livestreamer.nil?
+            cmd_output = `livestreamer #{stream_url}`
+         else
+            cmd_output = `#{path_to_livestreamer} #{stream_url}`
+         end
+
+         # TODO: manage error case where livestreamer won't even work
+
+         raise LauncherException if cmd_output.include?("error:")
+
+         # r1 cleans the output to get only the qualities separated by some characters...
+         # r2 matches these characters to create and return an array containing the qualities
+         r1 = Regexp.new(".+\sAvailable streams: ")
+         r2 = Regexp.new("(\s\(\w+\)(\, )?)|(\, )")
+         cmd_output.slice!(r1).split(r2)
       end
 
-      # TODO: manage error case where livestreamer won't even work
-
-      raise StreamLauncherException if cmd_output.include?("error:")
-
-      # r1 cleans the output to get only the qualities separated by some characters...
-      # r2 matches these characters to create and return an array containing the qualities
-      r1 = Regexp.new(".+\sAvailable streams: ")
-      r2 = Regexp.new("(\s\(\w+\)(\, )?)|(\, )")
-      cmd_output.slice!(r1).split(r2)
-   end
-
-   def self.launch_stream(stream_url, quality, path_to_livestreamer = nil)
-      if path_to_livestreamer.nil?
-         pid = spawn("livestreamer #{stream_url} #{quality}")
-      else
-         pid = spawn("#{path_to_livestreamer} #{stream_url} #{quality}")
+      def launch_stream(stream_url, quality, path_to_livestreamer = nil)
+         if path_to_livestreamer.nil?
+            pid = spawn("livestreamer #{stream_url} #{quality}")
+         else
+            pid = spawn("#{path_to_livestreamer} #{stream_url} #{quality}")
+         end
+         Process.detach pid
       end
-      Process.detach pid
-   end
 
-   def self.launch_chat(chat_url, path_to_browser)
-      pid = spawn("#{path_to_browser} #{chat_url}")
-      Process.detach pid
-   end
-
-   def self.record(stream_url, quality, path_to_livestreamer = nil, path_to_recording_directory = ".")
-      if path_to_livestreamer.nil?
-         pid = spawn("livestreamer #{stream_url} #{quality} -o #{path_to_recording_directory}")
-      else
-         pid = spawn("#{path_to_livestreamer} #{stream_url} #{quality} -o #{path_to_recording_directory}")
+      def launch_chat(chat_url, path_to_browser)
+         pid = spawn("#{path_to_browser} #{chat_url}")
+         Process.detach pid
       end
-      Process.detach pid
+
+      def record(stream_url, quality, path_to_livestreamer = nil, path_to_recording_directory = ".")
+         if path_to_livestreamer.nil?
+            pid = spawn("livestreamer #{stream_url} #{quality} -o #{path_to_recording_directory}")
+         else
+            pid = spawn("#{path_to_livestreamer} #{stream_url} #{quality} -o #{path_to_recording_directory}")
+         end
+         Process.detach pid
+      end
    end
 
 =begin USELESS FOR THE MOMENT, see is_stream_online?
